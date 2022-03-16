e90cdc5c1ccdc695cc90707fa5362405edf19c41
sebug <sebastian.gfeller@gmail.com>
2022-03-16 17:44:13 +0100

    Barcode values send picture

--- MaterialInOut/Controllers/BarCodeValuesController.cs
+++ MaterialInOut/Controllers/BarCodeValuesController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ZXing.PDF417;
+using ZXing.ImageSharp.Rendering;
+using ZXing.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Formats.Png;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,9 +18,19 @@ namespace MaterialInOut.Controllers
     {
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(string id)
+        public FileContentResult Get(string id)
         {
-            return id;
+            var writer = new BarcodeWriter<Rgba32>
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE
+            };
+            var image = writer.Write(id);
+            var pngEncoder = new PngEncoder();
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, pngEncoder);
+                return File(ms.ToArray(), "image/png");
+            }
         }
     }
 }
