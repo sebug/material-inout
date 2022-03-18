52ee3191c64bd3cd537fb9780847344fbfe4aa4e
sebug <sebastian.gfeller@gmail.com>
2022-03-18 16:04:36 +0100

    PDF 417

--- MaterialInOut/Controllers/BarCodeValuesController.cs
+++ MaterialInOut/Controllers/BarCodeValuesController.cs
@@ -8,6 +8,8 @@ using ZXing.ImageSharp.Rendering;
 using ZXing.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Formats.Png;
+using ZXing.QrCode.Internal;
+using ZXing.QrCode;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,10 +22,18 @@ namespace MaterialInOut.Controllers
         [HttpGet("{id}")]
         public FileContentResult Get(string id)
         {
+            var options = new PDF417EncodingOptions
+            {
+                CharacterSet = "UTF-8",
+                Width = 400,
+                Height = 200
+            };
             var writer = new BarcodeWriter<Rgba32>
             {
-                Format = ZXing.BarcodeFormat.QR_CODE
+                Format = ZXing.BarcodeFormat.PDF_417,
+                Options = options
             };
+            
             var image = writer.Write(id);
             var pngEncoder = new PngEncoder();
             using (var ms = new MemoryStream())
